30744d3b2f69aedb011c430a71690e380f332b81
Rona <hexinyirona@gmail.com>
2022-12-07 14:10:28 +0800

    server+client code 1st version finished

--- client.py
+++ client.py
@@ -1,3 +1,60 @@
 from socket import *
-from threading import *
+import threading
 from tkinter import *
+
+
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 5000
+
+
+def start(sock):
+    while True:
+        global username
+        log_option = input("Choose LOGIN or SIGNUP:")
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
+        send_msg = ("{}+{}+{}".format(log_option, username, password))
+        sock.send(send_msg.encode('utf-8'))
+        data = sock.recv(4096).decode("utf-8")
+        if not data:
+            return
+        status, message = data.strip().split(":")
+        print(data)
+        if status == "Succeed_1":
+            t1 = threading.Thread(target=reader, args=[sock])
+            t2 = threading.Thread(target=writer, args=[sock])
+            t1.start()
+            t2.start()
+            t1.join()
+            t2.join()
+        elif status == "Succeed_2":
+            pass
+        elif status == "Error":
+            pass
+        else:
+            raise Exception("Not defined status!", status)
+
+
+def reader(sock: socket):
+    while True:
+        data = sock.recv(4096).decode('utf-8')
+        if not data:
+            break
+        print(data)
+
+def writer(sock: socket):
+    while True:
+        data = input("").encode('utf-8')
+        sock.send(data)
+
+if __name__ == "__main__":
+    try:
+        clientSocket = socket(AF_INET, SOCK_STREAM)
+        clientSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        clientSocket.connect((SERVER_IP, SERVER_PORT))
+        start(clientSocket)
+
+    except Exception as msg:
+        print("Connection establishment in client " + str(msg))
+        print(msg)
+
--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 from socket import *
 import threading
-
+import traceback
 
 SERVER_IP = "127.0.0.1"
-SERVER_PORT = "5000"
+SERVER_PORT = 5000
 # Dictionary of accounts and password
 accounts = {}
 # List of logged-in user_name
@@ -18,41 +18,40 @@ mutex = threading.Lock()
 CHAT_FILE = "chat_history.txt"
 
 
-def create_connection(sock, addr):
+def authentication(sock, addr):
     while True:
         try:
             data = sock.recv(4096).decode("utf-8")
             if not data:
                 return
-            log_option, username, password = data.decode().strip("+")
+            log_option, username, password = data.strip().split("+")
 
-            if log_option == "LOGIN":
+            if log_option == "l":
                 if username in accounts:
                     if accounts[username] == password:
-                        sock.send("Successfully log in!".encode("utf-8"))
+                        sock.send("Succeed_1:Successfully log in!".encode("utf-8"))
                         user_logged.append(username)
                         connections.append(sock)
                         # Start message-handling thread
                         threading.Thread(target=handle_message, args=(sock, addr, username), daemon=True).start()
-                        # return??
+                        return
                     else:
-                        sock.send("Error: Incorrect password. Try again".encode("utf-8"))
+                        sock.send("Error:Incorrect password. Try again".encode("utf-8"))
                         continue
                 else:
-                    sock.send("Error: Incorrect username. Try again or sign up first".encode("utf-8"))
+                    sock.send("Error:Incorrect username. Try again or sign up first".encode("utf-8"))
                     continue
 
-            elif log_option == "SIGNUP":
+            elif log_option == "s":
                 if username in accounts:
-                    sock.send("Error: The username has already been used, please use another one.".encode("utf-8"))
+                    sock.send("Error:The username has already been used, please use another one.".encode("utf-8"))
                     continue
                 else:
                     accounts[username] = password
-                    sock.send("User account has been successfully created! Now you can log in!")
+                    sock.send("Succeed_2:User account has been successfully created! Now you can log in!".encode("utf-8"))
                     continue
-
-        except Exception as msg:
-            print(msg)
+        except Exception as msg_1:
+            traceback.print_exc()
             sock.close()
 
 
@@ -74,29 +73,34 @@ def handle_message(sock, addr, username):
                 # Broadcast the message to all connected clients
                 broadcast_message(f"{username} from {addr}: {data}")
 
-        except Exception as msg:
-            print(msg)
+        except Exception as msg_2:
+            print("Handling message in server:" + str(msg_2))
             sock.close()
 
 
 # Function to broadcast a message to all connected clients
 def broadcast_message(msg):
+    print("broadcast")
     for conn in connections:
         conn.send(msg.encode("utf-8"))
 
 
-# Creat a server socket
-serverSocket = socket(AF_INET, SOCK_STREAM)
-# Keep the port ready to be reused soon after close connection
-serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-# Bind the ip address and port number to socket with socket descriptor
-serverSocket.bind((SERVER_IP, SERVER_PORT))
-# Listen for clients' request; the parameter controls the number of concurrent connections that have not been accept()
-serverSocket.listen(5)
-print("Waiting for connection from clients...")
+try:
+    # Creat a server socket
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    # Keep the port ready to be reused soon after close connection
+    serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    # Bind the ip address and port number to socket with socket descriptor
+    serverSocket.bind((SERVER_IP, SERVER_PORT))
+    # Listen for clients' request; the parameter controls the number of concurrent connections not accepted yet
+    serverSocket.listen(5)
+    print("Waiting for connection from clients...")
 
-while True:
-    clientSocket, clientAddr = serverSocket.accept()
-    print("Login/Signup page connections established from: ", clientAddr)
-    # daemon = True: allow main thread to exit any time without waiting other threads to exit
-    threading.Thread(target=create_connection, args=(clientSocket, clientAddr), daemon=True).start()
+    while True:
+        clientSocket, clientAddr = serverSocket.accept()
+        print("Login/Signup page connections established from: ", clientAddr)
+        # daemon = True: allow main thread to exit any time without waiting other threads to exit
+        thread = threading.Thread(target=authentication, args=(clientSocket, clientAddr), daemon=True)
+        thread.start()
+except Exception as msg_3:
+    print("Connection establishment in server: " + str(msg_3))
